94c14dc4f96b759f5f3693dd1f1102f9b9291e03
SaiKeshav <keshavkolluru@gmail.com>
2020-04-25 18:22:53 +0530

    cleaned up filter.py

--- imojie/aggregate/filter.py
+++ imojie/aggregate/filter.py
@@ -1,4 +1,3 @@
-# import thinqpbo as tq
 import pdb
 import json
 import argparse
@@ -6,12 +5,11 @@ import rouge
 from rouge import rouge_n_sentence_level
 import math
 import thinqpbo as tq
-# node_t = 0.65
-# edge_t = 0.85
+from tqdm import tqdm
+
 node_t = 0.65
 edge_t = 0.85
-# node_t = 0.1
-# edge_t = 0.003
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--inp_fp')
@@ -24,7 +22,7 @@ def qpbo(out_fp, sent_list, graphD, node_edgeD):
     # Number of nodes to add.
     f = open(out_fp, "w")
     cnt = 0
-    for i in range(len(sent_list)):
+    for i in tqdm(range(len(sent_list))):
         graph = tq.QPBODouble()
         nodes_to_add = len(graphD[sent_list[i]])
         num = nodes_to_add
@@ -32,12 +30,10 @@ def qpbo(out_fp, sent_list, graphD, node_edgeD):
         visit = dict()
         for j in range(num):
             node_score = node_edgeD[sent_list[i]][j]
-            # print(node_score)
             if node_score < node_t:
                 visit[j] = False
                 continue
             visit[j] = True
-            # print(graphD[sent_list[i]][j], node_score)
             graph.add_unary_term(j, 0, -node_edgeD[sent_list[i]][j])
         l1 = list(visit.values())
         if True in l1:
@@ -45,19 +41,14 @@ def qpbo(out_fp, sent_list, graphD, node_edgeD):
         for j in range(num):
             for k in range(j+1, num):
                 edge_score = node_edgeD[sent_list[i]][(j,k)]
-                # print(edge_score)
                 if not((visit[j] == True) and (visit[k] == True) and (edge_score > edge_t)):
                     continue
-                # print(j, k, edge_score)
                 graph.add_pairwise_term(j, k, 0, 0, 0, edge_score)      
         graph.solve()
-        #if i==0:
-        #   pdb.set_trace()
         graph.compute_weak_persistencies()
         twice_energy = graph.compute_twice_energy()
         for n in range(nodes_to_add):
             segment = graph.get_label(n)
-            # print(i,segment)
             if segment == 1:
                 f.write(sent_list[i] + '\t' + graphD[sent_list[i]][n] + '\t' + str(math.log(node_edgeD[sent_list[i]][n])) + '\n')
     # Add two nodes.
@@ -91,7 +82,6 @@ def get_data(inp_fp):
             graphD[key][cnt]=item[0]
             cnt += 1
     sent_list = []
-    # sent_dict = json.load(open('extractions.txt', 'r'))
     sent_dict = extD
     for key in sent_dict:
         sent_list.append(key)
@@ -99,40 +89,25 @@ def get_data(inp_fp):
         num = len(sent_dict[key])
         key_sum = 0
         for i in range(num):
-            # for j in range(num):
-            #     if sent_dict[key][i] == extD[key][j][0]:
-                    # key_sum += math.exp(float(extD[key][j][1]))
-            # node_edgeD[key][i] = math.exp(float(extD[key][i][1]))
             key_sum += math.exp(float(extD[key][i][1]))
 
         for i in range(num):
-            # for j in range(num):
-            #     if sent_dict[key][i] == extD[key][j][0]:
-            #         node_edgeD[key][i] = str((math.exp(float(extD[key][j][1])))/key_sum)
-            
-            # node_edgeD[key][i] = (math.exp(float(extD[key][i][1])))/key_sum
             node_edgeD[key][i] = (math.exp(float(extD[key][i][1])))
                     
         edge_sum = 0
         for i in range(0,num):
             for j in range(i+1, num):
-                # sent1 = sent_dict[key][i].split()
-                # sent2 = sent_dict[key][j].split()
                 sent1 = ''.join(sent_dict[key][i])
                 sent2 = ''.join(sent_dict[key][j])
                 
                 recall, precision, rouge = rouge_n_sentence_level(sent1, sent2, 2)
-                #node_edgeD[key][(i,j)] = rouge
                 edge_sum += rouge
         for i in range(0,num):
             for j in range(i+1, num):
-                # sent1 = sent_dict[key][i].split()
-                # sent2 = sent_dict[key][j].split()
                 sent1 = ''.join(sent_dict[key][i])
                 sent2 = ''.join(sent_dict[key][j])
                 recall, precision, rouge = rouge_n_sentence_level(sent1, sent2, 2)
 
-                # node_edgeD[key][(i,j)] = rouge/edge_sum
                 node_edgeD[key][(i,j)] = rouge
     return sent_list, extD, graphD, node_edgeD
     
@@ -141,11 +116,7 @@ def main():
     parser = parse_args()
     args = parser.parse_args()
     sent_list, extD, graphD, node_edgeD = get_data(args.inp_fp)
-    # print(node_edgeD)
-    # with open('data2.txt', 'w') as outfile:
-    #    json.dump(node_edgeD, outfile)
     qpbo(args.out_fp, sent_list, graphD, node_edgeD)
-    #pdb.set_trace()                            
 
 if __name__ == '__main__':
     main()  
